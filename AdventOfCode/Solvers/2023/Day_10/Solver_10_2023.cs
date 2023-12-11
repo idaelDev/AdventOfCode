cda5f0f5ec52cab0d097e787ea95229fa1073564
idaeldev <judicael.abecassis@gmail.com>
2023-12-11 12:13:19 +0100

    Solve 10 & 11 2023

--- AdventOfCode/Solvers/2023/Day_10/Solver_10_2023.cs
+++ AdventOfCode/Solvers/2023/Day_10/Solver_10_2023.cs
@@ -1,6 +1,7 @@
 using AdventOfCode.API;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,160 @@ namespace AdventOfCode
     //URL: https://adventofcode.com/2023/day/10
     internal class Solver_10_2023 : ISolver
     {
+        Dictionary<char, ConnectionMap> connections = new Dictionary<char, ConnectionMap>()
+        {
+            {'|', new ConnectionMap(){North = true, East = false, South = true, West = false} },
+            {'-', new ConnectionMap(){North = false, East = true, South= false, West = true} },
+            {'L', new ConnectionMap(){North = true, East = true, South= false, West = false} },
+            {'J', new ConnectionMap(){North = true, East = false, South= false, West = true} },
+            {'7', new ConnectionMap(){North = false, East = false, South= true, West = true} },
+            {'F', new ConnectionMap(){North = false, East = true, South= true, West = false} },
+            {'S', new ConnectionMap(){North = true, East = true, South= true, West = true} },
+            {'.', new ConnectionMap(){North = false, East = false, South= false, West = false} },
+        };
+
         public string SolvePart1(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            string[] grid = Tools.LineSplit(data);
+            //Find start
+            (int, int) start = grid.Where(l => l.Contains('S')).Select(l => (grid.ToList().IndexOf(l), l.IndexOf('S'))).First();
+            (int, int)[] connected = GetConnected(start, grid);
+            List<(int, int)> loop = new List<(int, int)>();
+            loop.Add(start);
+            loop.AddRange(connected);
+
+            (int, int)[] paths  = connected;
+
+            while(paths[0] != paths[1])
+            {
+                for (int i = 0; i < paths.Length; i++)
+                {
+                    connected = GetConnected(paths[i], grid);
+                    paths[i] = connected.First(c => !loop.Contains(c));
+                }
+                loop.AddRange(paths);
+            }
+
+            return ((loop.Count()-1)/2).ToString();
         }
 
         public string SolvePart2(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            string[] grid = Tools.LineSplit(data);
+            //Find start
+            (int, int) start = grid.Where(l => l.Contains('S')).Select(l => (grid.ToList().IndexOf(l), l.IndexOf('S'))).First();
+            (int, int)[] connected = GetConnected(start, grid);
+            List<(int, int)> loop = new List<(int, int)>();
+            loop.Add(start);
+            (int, int) current = start;
+
+            while (true)
+            {
+                var buf = GetConnected(current, grid).Where(p => !loop.Contains(p));
+                if (!buf.Any())
+                    break;
+                current = buf.First();
+                loop.Add(current);
+            }
+
+            loop = loop.Distinct().ToList();
+            List<(int, int)> polygon = new List<(int, int)>();
+            int index = 1;
+            polygon.Add(loop[0]);
+            while (index < loop.Count -1)
+            {
+                if (loop[index].Item2 == polygon.Last().Item2)
+                {
+                    while(loop[index].Item2 == polygon.Last().Item2 && index < loop.Count -1)
+                    {
+                        index++;
+                    }
+                    polygon.Add(loop[index - 1]);
+                }
+                else if (loop[index].Item1 == polygon.Last().Item1)
+                {
+                    while (loop[index].Item1 == polygon.Last().Item1 && index < loop.Count - 1)
+                    {
+                        index++;
+                    }
+                    polygon.Add(loop[index - 1]);
+                }
+            }
+
+            
+            //GetBounds
+            (int, int) minBounds = (polygon.MinBy(x => x.Item1).Item1, polygon.MinBy(x => x.Item2).Item2);
+            (int, int) maxBounds = (polygon.MaxBy(x => x.Item1).Item1, polygon.MaxBy(x => x.Item2).Item2);
+
+            int count = 0;
+            for (int l = minBounds.Item1; l <= maxBounds.Item1; l++)
+            {
+                for (int c = minBounds.Item2; c <= maxBounds.Item2; c++)
+                {
+                    if (!loop.Contains((l,c)))
+                    {
+                        if(IsPointInsidePolygon((l,c), polygon))
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count.ToString();
         }
+
+        internal (int, int)[] GetConnected((int, int) p, string[] grid)
+        {
+            List<(int, int)> connected = new List<(int, int)>();
+            char current = grid[p.Item1][p.Item2];
+            if(p.Item1 > 0 && connections[current].North && connections[grid[p.Item1 - 1][p.Item2]].South)
+            {
+                connected.Add((p.Item1 - 1, p.Item2));
+            }
+            if(p.Item1 < grid.Length -1 && connections[current].South && connections[grid[p.Item1 + 1][p.Item2]].North) 
+            {
+               connected.Add((p.Item1 + 1, p.Item2));
+            }
+            if (p.Item2 > 0 && connections[current].West && connections[grid[p.Item1][p.Item2 -1]].East)
+            {
+                connected.Add((p.Item1, p.Item2-1));
+            }
+            if (p.Item2 < grid[p.Item1].Length-1 && connections[current].East && connections[grid[p.Item1][p.Item2 +1]].West)
+            {
+                connected.Add((p.Item1, p.Item2 + 1));
+            }
+            return connected.ToArray();
+        }
+
+        // Fonction pour vérifier si un point est à l'intérieur d'un polygone
+        internal bool IsPointInsidePolygon((int, int) point, List<(int, int)> polygon)
+        {
+            int count = 0;
+            int n = polygon.Count;
+
+            for (int i = 0, j = n - 1; i < n; j = i++)
+            {
+                if (((polygon[i].Item2 <= point.Item2 && point.Item2 < polygon[j].Item2) || (polygon[j].Item2 <= point.Item2 && point.Item2 < polygon[i].Item2)))
+                {
+                    if ((point.Item1 < (polygon[j].Item1 - polygon[i].Item1) * (point.Item2 - polygon[i].Item2) / (polygon[j].Item2 - polygon[i].Item2) + polygon[i].Item1))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count % 2 == 1;
+        }
+
+        internal struct ConnectionMap
+        {
+            public bool North;
+            public bool East;
+            public bool South;
+            public bool West;
+
+        }
+
     }
 }
--- AdventOfCode/Solvers/2023/Day_11/Solver_11_2023.cs
+++ AdventOfCode/Solvers/2023/Day_11/Solver_11_2023.cs
@@ -12,12 +12,82 @@ namespace AdventOfCode
     {
         public string SolvePart1(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            List<string> lines = Tools.LineSplit(data).ToList();
+            List<int> empltyLines = lines.Select((s,i) => i).Where(i => !lines[i].Contains('#')).ToList();
+            List<int> emptyColumns = lines.Select((s,x) => s.Select((c, i) => i).Where(y => lines[x][y] == '.')).Aggregate((a,b) => a.Intersect(b)).ToList();
+
+            for (int i = 0; i < emptyColumns.Count; i++)
+            {
+                lines = lines.Select(s => s.Insert(emptyColumns[i]+i, ".")).ToList();
+            }
+            string empty = lines[empltyLines[0]];
+            for (int i = 0; i < empltyLines.Count; i++)
+            {
+                lines.Insert(empltyLines[i]+i, empty);
+            }
+
+            List<(int, int)> galaxies = lines.SelectMany((s, x) => s.Select((c, i) => i).Where(y => lines[x][y] == '#').Select((a) => (x, a))).ToList();
+
+            List<int> distances = new List<int>();
+
+            for (int i = 0; i < galaxies.Count-1; i++)
+            {
+                for (int j = i+1; j < galaxies.Count; j++)
+                {
+                    distances.Add(GetManhattan(galaxies[i], galaxies[j]));
+                }
+            }
+
+           
+            return distances.Sum().ToString();
         }
 
         public string SolvePart2(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            List<string> lines = Tools.LineSplit(data).ToList();
+            List<int> empltyLines = lines.Select((s, i) => i).Where(i => !lines[i].Contains('#')).ToList();
+            List<int> emptyColumns = lines.Select((s, x) => s.Select((c, i) => i).Where(y => lines[x][y] == '.')).Aggregate((a, b) => a.Intersect(b)).ToList();
+            List<(int, int)> galaxies = lines.SelectMany((s, x) => s.Select((c, i) => i).Where(y => lines[x][y] == '#').Select((a) => (x, a))).ToList();
+            List<long> distances = new List<long>();
+            for (int i = 0; i < galaxies.Count - 1; i++)
+            {
+                for (int j = i + 1; j < galaxies.Count; j++)
+                {
+                    distances.Add(GetManhattanExpansion(galaxies[i], galaxies[j], empltyLines, emptyColumns));
+                }
+            }
+
+
+            return distances.Sum().ToString();
         }
+
+        internal long GetManhattanExpansion((int, int) g1, (int, int) g2, List<int> emptyLine, List<int> emptyColumns)
+        {
+            int factor = 1000000;
+            factor--;
+            (int, int) minMaxLine = (Math.Min(g1.Item1, g2.Item1), Math.Max(g1.Item1, g2.Item1));
+            (int, int) minMaxColumn = (Math.Min(g1.Item2, g2.Item2), Math.Max(g1.Item2, g2.Item2));
+            int emptyLinesCount = emptyLine.Where(l => l > minMaxLine.Item1 && l < minMaxLine.Item2).Count();
+            int emptyColumnsCount = emptyColumns.Where(l => l > minMaxColumn.Item1 && l < minMaxColumn.Item2).Count();
+
+            long m = Math.Abs((minMaxLine.Item2 + (factor*emptyLinesCount)) - minMaxLine.Item1) + Math.Abs((minMaxColumn.Item2 + (factor * emptyColumnsCount)) - minMaxColumn.Item1);
+
+            return m;
+        }
+
+        internal int GetManhattan((int,int) g1, (int, int) g2)
+        {
+            return Math.Abs(g1.Item1 - g2.Item1) + Math.Abs(g1.Item2 - g2.Item2);
+        }
+
+        internal void DrawLines(List<string> lines)
+        {
+            Console.WriteLine("Lines");
+            foreach (var item in lines)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
     }
 }

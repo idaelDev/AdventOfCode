fd9fb040d9701df4f2e2cfb16a4d4b1613b1d002
Judicael Abecassis <judicael.abecassis@gmail.com>
2024-12-02 12:06:32 +0100

    Solve 2024 Day 2

--- AdventOfCode/Solvers/2024/Day_2/Solver_2_2024.cs
+++ AdventOfCode/Solvers/2024/Day_2/Solver_2_2024.cs
@@ -12,12 +12,59 @@ namespace AdventOfCode
     {
         public string SolvePart1(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            List<int[]> reports = Tools.LineSplit(data).Select(x => x.Split(' ').Select(x => int.Parse(x)).ToArray()).ToList();
+
+            //var result = reports.Select(r => r.Zip(r.Skip(1), (x, y) => x - y))
+            //    .Where(r => r.Count(x => x > 0) == r.Count() || r.Count(x => x<0) == r.Count())
+            //    .Where(r => r.Count(x => Math.Abs(x) <= 3) == r.Count()).Count();
+
+            var result = reports.Where(r => IsValid(r)).Count();
+
+            return result.ToString();
         }
 
         public string SolvePart2(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            List<int[]> reports = Tools.LineSplit(data).Select(x => x.Split(' ').Select(x => int.Parse(x)).ToArray()).ToList();
+
+            int count = 0;
+            foreach (var r in reports)
+            {
+                bool pass = false;
+                for (int i = 0; i < r.Length; i++)
+                {
+                    var subReport = r.Where((x, j) => j != i).ToArray();
+                    if(IsValid(subReport))
+                    {
+                        pass = true;
+                    }
+                }
+                if (pass)
+                {
+                    count++;
+                }
+            }
+
+            return count.ToString();
         }
+        
+        private bool IsValid(int[] r)
+        {
+            return IsIncreasing(r) && IsProgressive(r);
+        }
+
+        private bool IsIncreasing(int[] r)
+        {
+            var sum = r.Zip(r.Skip(1), (x, y) => x - y).ToList();
+            return sum.Count(x => x > 0) == sum.Count() || sum.Count(x => x < 0) == sum.Count();
+        }
+
+        private bool IsProgressive(int[] r)
+        {
+            var sum = r.Zip(r.Skip(1), (x, y) => x - y).ToList();
+            return sum.Count(x => Math.Abs(x) <= 3) == sum.Count();
+        }
+
+
     }
 }

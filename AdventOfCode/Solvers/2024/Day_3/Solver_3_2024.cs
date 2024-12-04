ae0cc990a76dd36d1f3c05bcb0d5e0acd0f8d8ea
Judicael Abecassis <judicael.abecassis@gmail.com>
2024-12-04 09:27:40 +0100

    Solve 2024 Day 4

--- AdventOfCode/Solvers/2024/Day_3/Solver_3_2024.cs
+++ AdventOfCode/Solvers/2024/Day_3/Solver_3_2024.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode
 {
@@ -12,12 +13,38 @@ namespace AdventOfCode
     {
         public string SolvePart1(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            string regex = @"mul\(\d{1,3},\d{1,3}\)";
+            var result = Regex.Matches(data, regex)
+                .Select(mc => int.Parse(mc.Value.Split(',')[0].Split('(')[1]) * int.Parse(mc.Value.Split(',')[1].Split(')')[0])).Sum();
+            return result.ToString();
         }
 
         public string SolvePart2(string data, Action<float> progress, Action<string> debug)
         {
-            return "unsolved";
+            string regex = @"(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))";
+            var matchs = Regex.Matches(data, regex);
+            
+            bool isEnabled = true;
+            int sum = 0;
+            foreach (Match match in matchs)
+            {
+                if(match.Value == "don't()")
+                {
+                    isEnabled = false;
+                }
+                else if(match.Value == "do()")
+                {
+                    isEnabled = true;
+                }
+                else if(isEnabled)
+                {
+
+                    sum += int.Parse(match.Value.Split(',')[0].Split('(')[1]) * int.Parse(match.Value.Split(',')[1].Split(')')[0]);
+                }
+
+            }
+
+            return sum.ToString();
         }
     }
 }
